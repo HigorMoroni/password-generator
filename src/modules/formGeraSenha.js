c115125fdb79fe260f4c2da4c8fa1059c9453df1
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-05-08 18:11:23 -0300

    Adicionando copy na senha
    Arrumando o problema do icone

--- src/modules/formGeraSenha.js
+++ src/modules/formGeraSenha.js
@@ -7,10 +7,16 @@ const chkMinusculas = document.querySelector('.chk-minusculas')
 const chkNumeros = document.querySelector('.chk-numeros')
 const chkSimbolos = document.querySelector('.chk-simbolos')
 const btnGerarSenha = document.querySelector('.gerar-senha')
+const btnCopiar = document.querySelector('i.fa-copy')
 
 export default () => {
     btnGerarSenha.addEventListener('click', () => {
         senhaGerada.innerHTML = gera()
+    })
+    btnCopiar.addEventListener('click', () => {
+        senhaGerada.select()
+        document.execCommand('copy')
+        alert('Senha copiada para a área de transferência!')
     })
 }
 
