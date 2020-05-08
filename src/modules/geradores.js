cb4a8727e2ae247b184304b581514d9a696ad1dc
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-05-08 19:22:32 -0300

    branch funcionando ok
    Pronta para marge com a master

--- src/modules/geradores.js
+++ src/modules/geradores.js
@@ -2,20 +2,24 @@ const rand = (min, max) => Math.floor(Math.random()*(max-min)+min)
 const simbolos = `'"!@#$%¨&*()_+-=´[]{}~^:;.,/?°ºª§£¢¬|`
 
 const geradores = [
-    () => String.fromCharCode(rand(65, 91)),
-    () => String.fromCharCode(rand(97, 123)),
-    () => String.fromCharCode(rand(48, 58)),
-    () => simbolos[rand(0, simbolos.length)]
+    () => String.fromCharCode(rand(65, 91)), //0 - Maiuscula
+    () => String.fromCharCode(rand(97, 123)), //1 - Minuscula
+    () => String.fromCharCode(rand(48, 58)), //2 - Numero
+    () => simbolos[rand(0, simbolos.length)] //3 - Simbolo
 ]
 
 
-export default function geraSenha(qtd, mai, min, num, sim) {
+export default function geraSenha(qtd) {
     const senhaArray = []
     qtd = Number(qtd)
-    for(let i=0;i<qtd;i++) {
+    while (senhaArray.length<qtd) {
         const caract = rand(0,5)
+        console.log(caract)
         const podeGerar = arguments[(caract+1)] ? true : false
-        if (podeGerar) senhaArray.push(geradores[caract])
+        console.log(podeGerar)
+        if (podeGerar) senhaArray.push(geradores[caract]())
+        console.log(senhaArray)
+        console.log(senhaArray.length)
     }
     return senhaArray.join('')
 }
