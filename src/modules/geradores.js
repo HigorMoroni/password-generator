30bf17717b382a721d19a4ff01e184ab5ac3afd8
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-05-08 18:57:26 -0300

    primeira tentativa
    fiz um array com os geradores e verifico se posso usar ele em cada volta do laço

--- src/modules/geradores.js
+++ src/modules/geradores.js
@@ -1,19 +1,21 @@
 const rand = (min, max) => Math.floor(Math.random()*(max-min)+min)
 const simbolos = `'"!@#$%¨&*()_+-=´[]{}~^:;.,/?°ºª§£¢¬|`
 
-const geraMaiuscula = () => String.fromCharCode(rand(65, 91))
-const geraMinuscula = () => String.fromCharCode(rand(97, 123))
-const geraNumero = () => String.fromCharCode(rand(48, 58))
-const geraSimbolo = () => simbolos[rand(0, simbolos.length)]
+const geradores = [
+    () => String.fromCharCode(rand(65, 91)),
+    () => String.fromCharCode(rand(97, 123)),
+    () => String.fromCharCode(rand(48, 58)),
+    () => simbolos[rand(0, simbolos.length)]
+]
+
 
 export default function geraSenha(qtd, mai, min, num, sim) {
     const senhaArray = []
     qtd = Number(qtd)
     for(let i=0;i<qtd;i++) {
-        mai && senhaArray.push(geraMaiuscula())
-        min && senhaArray.push(geraMinuscula())
-        num && senhaArray.push(geraNumero())
-        sim && senhaArray.push(geraSimbolo())
+        const caract = rand(0,5)
+        const podeGerar = arguments[(caract+1)] ? true : false
+        if (podeGerar) senhaArray.push(geradores[caract])
     }
-    return senhaArray.join('').slice(0,qtd)
+    return senhaArray.join('')
 }
